42219f190a8f74db325ffd59b2550784850a8ad5
piash51 <piashp305@gmail.com>
2026-09-22 12:13:59 +0600

    update otp

--- services/customerEmailService.js
+++ services/customerEmailService.js
@@ -3,7 +3,11 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  family: 4,
   auth: {
     user: process.env.SMTP_EMAIL,
     pass: process.env.SMTP_APP_PASSWORD,
